9543466e2086c49ce4cac25da7e2e879ed749709
Smonique <121874599+Smonique@users.noreply.github.com>
2026-01-20 18:43:10 +0400

    blocks

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
-const banner = document.getElementById("banner")[0];
-const blocks = document.getElementsByClassName("blocks");
-
+const banner = document.getElementsByClassName('banner')[0];
+const blocks = document.getElementsByClassName('blocks');
+let content = "";
 for (let i = 1; i < 400; i++) {
-  banner.innerHTML += "<div class='blocks'></div>";
+  content += "<div class='blocks'></div>";
 }
+
+banner.innerHTML = content;
