42bd4e6510944d1e8bcbefef8bb4c0f3e8b36201
Smonique <121874599+Smonique@users.noreply.github.com>
2026-01-20 20:51:56 +0400

    image

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-const banner = document.getElementsByClassName("banner")[0];
-const blocks = document.getElementsByClassName("blocks");
-for (let i = 1; i < 400; i++) {
+const banner = document.getElementsByClassName('banner')[0];
+const blocks = document.getElementsByClassName('blocks');
+for (let i = 1; i < 300; i++) {
   banner.innerHTML += "<div class='blocks'></div>";
+  blocks[i].style.animationDelay = `${i * 0.05}s`;
 }
-alert("Hello! JS is connected.");
